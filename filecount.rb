2f41df300572cc38eed849a0a5650f9fbbf861c2
Andrew Hay <andrewsmhay@gmail.com>
2013-04-30 14:26:37 -0700

    Added OS detection
    Added operating system detection to recurse directories properly.

--- filecount.rb
+++ filecount.rb
@@ -1,9 +1,37 @@
-#!/bin/usr/ruby
+require 'rbconfig'
+
+def os 
+    @os ||= (
+      host_os = RbConfig::CONFIG['host_os']
+      case host_os
+      when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+        :windows
+      when /darwin|mac os|linux|solaris|bsd/
+        :nix
+      else
+        raise Error::WebDriverError, "unknown os: #{host_os.inspect}"
+     end
+)
+end
 
 directories = Array.new
-directories = Dir.glob('/'"**/")
-directories.sort!
-directories.each do |countycount|
-	filecount = Dir.glob(File.join(countycount, '**', '*')).select { |file| File.file?(file) }.count
-	puts "#{countycount}|#{filecount}"
+
+if os == :nix 
+  puts "*nix directory|count"
+  # directories = Dir.glob('/'"**/")
+  directories = ['/etc']
+  directories.sort!
+  directories.each do |countycount|
+    filecount = Dir.glob(File.join(countycount, '**', '*')).select { |file| File.file?(file) }.count
+    puts "#{countycount}|#{filecount}"
+  end
+else
+  puts "Windows directory|count"
+  # directories = Dir.glob('C:\'"**/")   
+  directories = ['C:\\']
+  directories.sort!
+  directories.each do |countycount|
+   filecount = Dir.glob(File.join(countycount, '**', '*')).select { |file| File.file?(file) }.count
+   puts "#{countycount}|#{filecount}"
+  end
 end
