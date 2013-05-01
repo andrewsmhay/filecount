98f8701eece39cc58caff8ab5dda26afc135d8ab
Andrew Hay <andrewsmhay@gmail.com>
2013-05-01 09:43:51 -0700

    Added inline comments to code

--- filecount.rb
+++ filecount.rb
@@ -1,5 +1,6 @@
 require 'rbconfig'
 
+# Detects the operating system that the script is being run on
 def os 
     @os ||= (
       host_os = RbConfig::CONFIG['host_os']
@@ -14,22 +15,35 @@ def os
 )
 end
 
+# Initializes the array
 directories = Array.new
 
+# If the operating system is a Unix, Linux, OS X, or other BSD system
 if os == :nix 
+# Put the header row
   puts "*nix directory|count"
+# Glob the directory
   directories = Dir.glob('/'"**/")
+# Sort the list of directories
   directories.sort!
+# Count the number of files, per directory
   directories.each do |countycount|
     filecount = Dir.glob(File.join(countycount, '**', '*')).select { |file| File.file?(file) }.count
+# Print the directory and count of files in '|' delimited format
     puts "#{countycount}|#{filecount}"
   end
+# If the operating system is a Windows system
 else
+# Header row
   puts "Windows directory|count"
+# Glob the directory
   directories = Dir.glob("/**/")   
+# Sort the list of directories
   directories.sort!
+# Count the number of files, per directory
   directories.each do |countycount|
     filecount = Dir.glob(File.join(countycount, '**', '*')).select { |file| File.file?(file) }.count
-    puts "#{countycount}|#{filecount}"
+# Print the directory and count of files in '|' delimited format
+  puts "#{countycount}|#{filecount}"
   end
 end
